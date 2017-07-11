e9c1c58281f26a9bc1b50fbefaf3eea30c2d7baa
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-07-11 18:25:12 +0100

    Introduction.Ex12: String Permutations

--- Introduction/Example12.cs
+++ Introduction/Example12.cs
@@ -1,9 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Introduction
 {
     public class Example12
     {
-        
+        public List<string> GetPermutations(string str)
+        {
+            List<string> results = new List<string>();
+            Permute(str, "", results);
+
+            return results;
+        }
+
+        public string Permute(string str, string prefix, List<string> results)
+        {
+            if (str.Length == 0)
+            {
+                results.Add(prefix);
+                return prefix;
+            }
+
+            string newString = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                string modified = str.Substring(0, i) + str.Substring(i + 1);
+                newString = Permute(modified, prefix + ch, results);
+            }
+
+            return newString;
+        }
     }
 }
--- test-ctci/Introduction/Test_Example12.cs
+++ test-ctci/Introduction/Test_Example12.cs
@@ -1,8 +1,22 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Introduction;
 
 namespace test_ctci
 {
-    
+    public class Test_Exmaple12
+    {
+        private Example12 example = new Example12();
+
+        [Fact]
+        public void FindPermutations()
+        {
+            List<string> results = example.GetPermutations("abc");
+
+            Assert.Equal(6, results.Count);
+            Assert.Equal("abc", results[0]);
+            Assert.Equal("bac", results[2]);
+        }
+    }
 }
