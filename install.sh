e28d068c10e53838d41ea1c993f82db0cd50e657
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-07-11 15:08:33 +0100

    Update installation script

--- install.sh
+++ install.sh
@@ -5,4 +5,12 @@ dotnet sln add test-ctci/test-ctci.csproj
 dotnet add test-ctci/test-ctci.csproj reference "CH 01. Arrays and Strings"/"CH 01. Arrays and Strings".csproj
 dotnet sln add ctci/ctci.csproj
 dotnet add ctci/ctci.csproj reference "CH 01. Arrays and Strings"/"CH 01. Arrays and Strings".csproj
+dotnet sln add "CH 02. Linked Lists"/"CH 02. Linked Lists".csproj
+dotnet add test-ctci/test-ctci.csproj reference "CH 02. Linked Lists"/"CH 02. Linked Lists".csproj
+dotnet sln add lib-ctci/lib-ctci.csproj
+dotnet add "CH 02. Linked Lists"/"CH 02. Linked Lists".csproj reference lib-ctci/lib-ctci.csproj
+dotnet add test-ctci/test-ctci.csproj reference lib-ctci/lib-ctci.csproj
+dotnet sln add "CH 04. Trees and Graphs"/"CH 04. Trees and Graphs".csproj
+dotnet add test-ctci/test-ctci.csproj reference "CH 04. Trees and Graphs"/"CH 04. Trees and Graphs".csproj
+dotnet add  "CH 04. Trees and Graphs"/"CH 04. Trees and Graphs".csproj reference lib-ctci/lib-ctci.csproj
 dotnet restore
