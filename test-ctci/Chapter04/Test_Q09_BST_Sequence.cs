46b5a3d39281fc8c137e237e26bea1cf3652eae6
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-07-12 12:42:06 +0100

    C4.Q9: BST Sequences: A binary search tree was created by traversing through an array from left to right and inserting each element. Given a binary search tree with distinct elements, print all possible arrays that could have led to this tree.

--- test-ctci/Chapter04/Test_Q09_BST_Sequence.cs
+++ test-ctci/Chapter04/Test_Q09_BST_Sequence.cs
@@ -13,7 +13,31 @@ namespace test_ctci
         [Fact]
         public void BuildSequence()
         {
-            
+            CtciTreeNode<int> node1 = CtciTreeNode<int>.CtciBinaryTreeNode(1);
+            CtciTreeNode<int> node2 = CtciTreeNode<int>.CtciBinaryTreeNode(2);
+            CtciTreeNode<int> node3 = CtciTreeNode<int>.CtciBinaryTreeNode(3);
+            CtciTreeNode<int> node4 = CtciTreeNode<int>.CtciBinaryTreeNode(4);
+            CtciTreeNode<int> node5 = CtciTreeNode<int>.CtciBinaryTreeNode(5);
+            CtciTreeNode<int> node6 = CtciTreeNode<int>.CtciBinaryTreeNode(6);
+
+            node4.Left = node2;
+            node4.Right = node6;
+
+            List<LinkedList<int>> results1 = sequence.BuildSequence(node4);
+
+            Assert.Equal(2, results1.Count);
+
+            node2.Left = node1;
+
+            List<LinkedList<int>> results2 = sequence.BuildSequence(node4);
+
+            Assert.Equal(3, results2.Count);
+
+            node2.Right = node3;
+
+            List<LinkedList<int>> results3 = sequence.BuildSequence(node4);
+
+            Assert.Equal(8, results3.Count);
         }
     }
 }
