ea93526845832fd742468623277a87ab92e2b676
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-07-24 16:32:38 +0100

    Greedy algorithm: Task sequence problem

--- Common Topics/Greedy Algorithms/Sequencing & Scheduling/Maximum_Number.cs
+++ Common Topics/Greedy Algorithms/Sequencing & Scheduling/Maximum_Number.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+// You are given n activities with their start and finish times. 
+// Select the maximum number of activities that can be performed by a single person, 
+// assuming that a person can only work on a single activity at a time.
+
 namespace Common_Topics
 {
     public static class Greedy_Maximum_Number
