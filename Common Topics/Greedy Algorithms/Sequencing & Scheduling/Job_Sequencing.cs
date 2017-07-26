70aaf141584f7a98f26f5f44b0dacd8451612696
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-07-26 16:06:48 +0100

    Greedy Algorithm-task scheduling problem

--- Common Topics/Greedy Algorithms/Sequencing & Scheduling/Job_Sequencing.cs
+++ Common Topics/Greedy Algorithms/Sequencing & Scheduling/Job_Sequencing.cs
@@ -22,7 +22,7 @@ namespace Common_Topics
                 return jobSequence;
             }
 
-            Array.Sort(jobs, new Sequencing_ReverseComparer());
+            Array.Sort(jobs, new Sequencing_ReverseComparerProfit());
 
             Sequencing_Job[] slots = new Sequencing_Job[jobs.Length];
             for (int i = 0; i < jobs.Length; i++)
@@ -65,7 +65,7 @@ namespace Common_Topics
                 return jobSequence;
             }
 
-            Array.Sort(jobs, new Sequencing_ReverseComparer());
+            Array.Sort(jobs, new Sequencing_ReverseComparerProfit());
 
             int maxDeadline = Int32.MinValue;
             foreach (Sequencing_Job job in jobs)
@@ -101,6 +101,71 @@ namespace Common_Topics
 
             return jobSequence;
         }
+
+        /*
+            The problem of scheduling unit-time tasks with deadlines and penalities for a single processor has following inputs
+            S={a1,a2,...,an} of n unit-time task. 
+            A unit-time task requires exactly 1 unit of time to complete
+            deadlines d1,d2,...,dn, 1<=di<=n
+            penalities w1,w2,...,wn. 
+            A penality wi is incurred if task ai is not finished by time di, and no penality if task finishes at deadline
+            The problem is to find a schedule for S that minimizes the total penalty incurred for missed deadlines
+
+            Complexety: O(n*log(n))
+         */
+        public static List<Tuple<Sequencing_Job, bool>> FindSequence3(Sequencing_Job[] jobs)
+        {
+            List<Tuple<Sequencing_Job, bool>> sequence = new List<Tuple<Sequencing_Job, bool>>();
+
+            if (jobs.Length <= 0)
+            {
+                return sequence;
+            }
+
+            Array.Sort(jobs, new Sequencing_ReverseComparerPenalty());
+
+            int maxDeadline = Int32.MinValue;
+            foreach (Sequencing_Job job in jobs)
+            {
+                if (job.Deadline > maxDeadline)
+                {
+                    maxDeadline = job.Deadline;
+                }
+            }
+
+            Sequencing_DisjointSet disjointSet = new Sequencing_DisjointSet(maxDeadline);
+            List<Sequencing_Job> penalised = new List<Sequencing_Job>();
+            Sequencing_Job[] tmpSequence = new Sequencing_Job[jobs.Length];
+            foreach (Sequencing_Job job in jobs)
+            {
+                int slot = disjointSet.Find(job.Deadline);
+
+                if (slot > 0)
+                {
+                    disjointSet.Merge(disjointSet.Find(slot - 1), slot);
+                    tmpSequence[slot - 1] = job;
+                }
+                else
+                {
+                    penalised.Add(job);
+                }
+            }
+
+            foreach (Sequencing_Job job in tmpSequence)
+            {
+                if (job != null)
+                {
+                    sequence.Add(new Tuple<Sequencing_Job, bool>(job, false));
+                }
+            }
+
+            foreach (Sequencing_Job job in penalised)
+            {
+                sequence.Add(new Tuple<Sequencing_Job, bool>(job, true));
+            }
+
+            return sequence;
+        }
     }
 
     public class Sequencing_Job : IComparable<Sequencing_Job>
@@ -108,12 +173,14 @@ namespace Common_Topics
         protected string name;
         protected int deadline;
         protected int profit;
+        protected int penalty;
 
-        public Sequencing_Job(string name, int deadline, int profit)
+        public Sequencing_Job(string name, int deadline, int profit, int penalty = 0)
         {
             this.name = name;
             this.deadline = deadline;
             this.profit = profit;
+            this.penalty = penalty;
         }
 
         int IComparable<Sequencing_Job>.CompareTo(Sequencing_Job obj)
@@ -146,9 +213,17 @@ namespace Common_Topics
                 return profit;
             }
         }
+
+        public int Penalty
+        {
+            get
+            {
+                return penalty;
+            }
+        }
     }
 
-    public class Sequencing_ReverseComparer : IComparer<Sequencing_Job>
+    public class Sequencing_ReverseComparerProfit : IComparer<Sequencing_Job>
     {
         int IComparer<Sequencing_Job>.Compare(Sequencing_Job x, Sequencing_Job y)
         {
@@ -156,14 +231,22 @@ namespace Common_Topics
         }
     }
 
+    public class Sequencing_ReverseComparerPenalty : IComparer<Sequencing_Job>
+    {
+        int IComparer<Sequencing_Job>.Compare(Sequencing_Job x, Sequencing_Job y)
+        {
+            return -1 * x.Penalty.CompareTo(y.Penalty);
+        }
+    }
+
     public class Sequencing_DisjointSet
     {
         protected int[] disjointSet;
 
-        public Sequencing_DisjointSet(int maxDeadline)
+        public Sequencing_DisjointSet(int slots)
         {
-            disjointSet = new int[maxDeadline + 1];
-            for (int i = 0; i <= maxDeadline; i++)
+            disjointSet = new int[slots + 1];
+            for (int i = 0; i <= slots; i++)
             {
                 disjointSet[i] = i;
             }
--- test-ctci/Common Topics/Greedy Algorithms/Sequencing & Scheduling/Test_Job_Sequencing.cs
+++ test-ctci/Common Topics/Greedy Algorithms/Sequencing & Scheduling/Test_Job_Sequencing.cs
@@ -44,5 +44,40 @@ namespace test_ctci
             Assert.Equal(job1, result[1]);
             Assert.Equal(job5, result[2]);
         }
+
+        [Fact]
+        public void FindSequence3()
+        {
+            Sequencing_Job job1 = new Sequencing_Job("a", 4, 0, 70);
+            Sequencing_Job job2 = new Sequencing_Job("b", 2, 0, 60);
+            Sequencing_Job job3 = new Sequencing_Job("c", 4, 0, 50);
+            Sequencing_Job job4 = new Sequencing_Job("d", 3, 0, 40);
+            Sequencing_Job job5 = new Sequencing_Job("e", 1, 0, 30);
+            Sequencing_Job job6 = new Sequencing_Job("f", 4, 0, 20);
+            Sequencing_Job job7 = new Sequencing_Job("g", 6, 0, 10);
+
+            List<Tuple<Sequencing_Job, bool>> result = Greedy_Sequencing.FindSequence3(new Sequencing_Job[] {
+                job1, job2, job3, job4, job5, job6, job7
+            });
+
+            Assert.True(result[1].Item1.Deadline > 1);
+            Assert.True(result[2].Item1.Deadline > 2);
+            Assert.True(result[3].Item1.Deadline > 3);
+            Assert.True(result[4].Item1.Deadline > 4);
+
+            int penalised = 0;
+            int fine = 0;
+            foreach (Tuple<Sequencing_Job, bool> tuple in result)
+            {
+                if (tuple.Item2)
+                {
+                    penalised++;
+                    fine += tuple.Item1.Penalty;
+                }
+            }
+
+            Assert.Equal(2, penalised);
+            Assert.Equal(50, fine);
+        }
     }
 }
