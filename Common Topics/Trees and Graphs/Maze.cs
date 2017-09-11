71dd8f67c2c0d75368b5ae3c3a531eba93152c09
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-09-11 17:00:35 +0100

    Rat in a maze

---
+++ Common Topics/Trees and Graphs/Maze.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Common_Topics
+{
+    public static class Maze
+    {
+        /*
+            A Maze is given as N*N binary matrix of blocks where source block is the upper left most block i.e., 
+            maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]. 
+            A rat starts from source and has to reach destination. The rat can move only in two directions: forward and down.
+         */
+        public static bool RatInMaze(int[,] maze, int x, int y, int[,] solution)
+        {
+            solution[x, y] = 1;
+
+            if (x == maze.GetUpperBound(0) && y == maze.GetUpperBound(0))
+            {
+                return true;
+            }
+
+            if (x + 1 <= maze.GetUpperBound(0) && maze[x + 1, y] == 1 && solution[x + 1, y] == 0)
+            {
+                if (RatInMaze(maze, x + 1, y, solution) == true)
+                {
+                    return true;
+                }
+            }
+
+            if (y + 1 <= maze.GetUpperBound(0) && maze[x, y + 1] == 1 && solution[x, y + 1] == 0)
+            {
+                if (RatInMaze(maze, x, y + 1, solution) == true)
+                {
+                    return true;
+                }
+            }
+
+            solution[x, y] = 0;
+
+            return false;
+        }
+    }
+}
---
+++ test-ctci/Common Topics/Trees and Graphs/Test_Maze.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using Common_Topics;
+
+namespace test_ctci
+{
+    public class Test_Maze
+    {
+        [Fact]
+        public void RatInMaze()
+        {
+            int[,] solution = new int[4, 4];
+            
+            Maze.RatInMaze(new int[,] {
+                {1, 0, 0, 0},
+                {1, 1, 0, 1},
+                {0, 1, 0, 0},
+                {1, 1, 1, 1}
+            }, 0, 0, solution);
+
+            Assert.Equal(new int[,] {
+                {1, 0, 0, 0},
+                {1, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 1, 1}
+            }, solution);
+        }
+    }
+}
