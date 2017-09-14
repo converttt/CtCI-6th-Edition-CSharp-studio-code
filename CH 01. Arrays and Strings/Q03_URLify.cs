74708a8b344ed58141dbfaae6c2bd244af0f04b3
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-09-14 18:42:56 +0100

    Add comment for Chapter1 Q03

--- CH 01. Arrays and Strings/Q03_URLify.cs
+++ CH 01. Arrays and Strings/Q03_URLify.cs
@@ -2,6 +2,11 @@ using System;
 
 namespace CH_01._Array_and_Strings
 {
+    /*
+        URLify: Write a method to replace all spaces in a string with '%20. 
+        You may assume that the string has sufficient space at the end to hold the additional characters, 
+        and that you are given the "true" length of the string. (Note: if implementing in Java, please use a character array so that you can perform this operation in place.)
+     */
     public class Q03_URLify
     {
         public string URLify1(string str, int initLength)
