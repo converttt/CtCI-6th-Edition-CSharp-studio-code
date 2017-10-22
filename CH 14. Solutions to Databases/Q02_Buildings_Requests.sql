1103fb123ac1150d9f83d64ba32bf42ecc058f1b
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-10-22 18:43:16 +0100

    Chanpter 14 Q02

--- CH 14. Solutions to Databases/Q02_Buildings_Requests.sql
+++ CH 14. Solutions to Databases/Q02_Buildings_Requests.sql
@@ -1,3 +1,6 @@
+-- Open Requests: Write a SQL query to get a list of all buildings and the number of open requests
+-- (Requests in which status equals 'Open').
+
 -- SOLUTION 1
 SELECT Buildings.*, COUNT(Requests.RequestID) AS ReqNum
 FROM Buildings
@@ -17,3 +20,15 @@ LEFT JOIN (
 ) AS Requests USING (AptID)
 GROUP BY Buildings.BuilidingID
 ORDER BY ReqNum DESC;
+
+-- SOLUTION 3
+SELECT Buildings.*, COALESCE(tmp.Requests, 0) AS Requests
+FROM Buildings
+LEFT JOIN (
+    SELECT Apartments.BuilidingID, count(*) AS Requests
+    FROM Apartments
+    LEFT JOIN Requests using(AptID)
+    WHERE Requests.Status = 'OPEN'
+    GROUP BY Apartments.BuilidingID
+) AS tmp using(BuilidingID)
+ORDER BY Requests DESC
