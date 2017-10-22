0de4f6d62431682a130c6ea7d7e741b894bc7f64
Andrey Kovsh <andrey.kovsh@gmail.com>
2017-10-22 16:33:00 +0100

    Add comments for Chapter 5 Q01.

--- CH 05. Bit Manipulations/Q01_Insertion.cs
+++ CH 05. Bit Manipulations/Q01_Insertion.cs
@@ -2,6 +2,13 @@
 
 namespace CH_05._Bit_Manipulations
 {
+    /*
+        Insertion: You are given two 32-bit numbers, Nand M, and two bit positions, i and j. 
+        Write a method to insert Minto Nsuch that Mstarts at bit j and ends at bit i. 
+        You can assume that the bits j through i have enough space to fit all of M. 
+        That is, if M= 18811, you can assume that there are at least 5 bits between j and i. 
+        You would not, for example, have j = 3 and i = 2, because Mcould not fully fit between bit 3 and bit 2.
+     */
     public class Q01_Insertion
     {
         public int Insert(int a, int b, int start, int end)
